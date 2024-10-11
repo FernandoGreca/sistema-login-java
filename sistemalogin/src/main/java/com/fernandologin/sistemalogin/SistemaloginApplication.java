42d053fea22087c0b24afffcf92f090695a83882
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-10-11 14:07:41 -0300

    Projeto SpringBoot criado

---
+++ sistemalogin/src/main/java/com/fernandologin/sistemalogin/SistemaloginApplication.java
@@ -0,0 +1,13 @@
+package com.fernandologin.sistemalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaloginApplication.class, args);
+	}
+
+}
---
+++ sistemalogin/src/test/java/com/fernandologin/sistemalogin/SistemaloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandologin.sistemalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
